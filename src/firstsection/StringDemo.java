35cfdbb69dd998a3273e2e430beb1dce5f33cae3
Ihor Chyzh <ichyzh@gmail.com>
2017-11-11 15:57:57 +0200

    Add variables + strings.

---
+++ src/firstsection/StringDemo.java
@@ -0,0 +1,25 @@
+package firstsection;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+        // String Literal - String Constant Pool
+        String str1 = "Hello";
+
+        String str3 = "Hello";
+
+        // String Object - Heap
+        String str2 = new String("Welcome");
+
+        String str4 = new String("Welcome");
+
+        // Strings
+
+        str1 = "More Hello";
+
+        String strAdd = str1 + " + " + str2;
+
+        System.out.println(strAdd);
+    }
+
+}
